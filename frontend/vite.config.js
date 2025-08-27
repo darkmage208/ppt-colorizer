cf30331b6013daabad59b7dccbd76564226eedb9
darkmage208 <harukaleonhart@gmail.com>
2025-08-27 07:30:40 -0700

    fix: add assets setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true
-  }
+  },
+  build: {
+    assetsDir: 'assets',
+    copyPublicDir: true
+  },
+  publicDir: 'public'
 })
